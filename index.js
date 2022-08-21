9db197a9a145077cd06b8818c93e1fdbc00d3835
ruthwekesa <wekesaruth569@gmail.com>
2022-08-21 15:11:10 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'not bob';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(modifiedBestCustomer) {
+    window.bestCustomer = modifiedBestCustomer;
+}
+
+function changeLeastFavoriteCustomer(modifiedLeastFavoriteCustomer) {
+    leastFavoriteCustomer = modifiedLeastFavoriteCustomer;
+}
